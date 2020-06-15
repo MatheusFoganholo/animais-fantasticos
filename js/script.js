95cbf0a097be023aa3564b2b5ceb4aea4fdda1fc
Matheus Foganholo <matheusfoganholo@hotmail.com>
2020-06-15 17:09:26 -0300

    Refactoring JS Code and Adding Comments to HTML index file

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 // Navigation By Tabs
 // Selecting List
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('active-tab')
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('active-tab')
 
-    // Adding class 'ativo' to the animal selected
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('active-tab');
+        // Adding class 'ativo' to the animal selected
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('active-tab');
+            });
+            tabContent[index].classList.add('active-tab')
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            })
         });
-        tabContent[index].classList.add('active-tab')
     }
-
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        })
-    });
 }
+
+// Init TabNav Function
+initTabNav();
