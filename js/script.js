64266f73e856ae6565b067fda78e4dc01dcb9ae7
Matheus Foganholo <matheusfoganholo@hotmail.com>
2020-06-22 17:37:36 -0300

    Refactoring js functions to use modules - ES6+

---
+++ js/script.js
@@ -0,0 +1,11 @@
+import initSmoothScroll from './modules/smooth-scroll.js';
+import initAnimeWhenScroll from './modules/anime-when-scroll.js';
+import initAccordion from './modules/accordion-list.js';
+import initTabNav from './modules/navigation-by-tabs.js';
+
+
+// Initializing Functions
+initSmoothScroll();
+initAnimeWhenScroll();
+initAccordion();
+initTabNav();
