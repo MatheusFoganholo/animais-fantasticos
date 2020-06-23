6397876a0009842dcafe998d968d19028562538d
Matheus Foganholo <matheusfoganholo@hotmail.com>
2020-06-23 13:55:31 -0300

    Modal part at js done. Still left the responsivity at css, it will be done after the project is conclude

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ import initTabNav from './modules/navigation-by-tabs.js';
 import initSmoothScroll from './modules/smooth-scroll.js';
 import initAccordion from './modules/accordion-list.js';
 import initAnimeWhenScroll from './modules/anime-when-scroll.js';
+import initModal from './modules/modal.js';
 
 // Initializing Functions
 initTabNav();
 initSmoothScroll();
 initAccordion();
 initAnimeWhenScroll();
+initModal();
