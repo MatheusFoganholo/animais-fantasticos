92df506f48e32a6cbaa7f84b860517bbfb49db9d
Matheus Foganholo <matheusfoganholo@hotmail.com>
2020-07-27 19:08:19 -0300

    Created Login Page

---
+++ js/login-script.js
@@ -0,0 +1,21 @@
+// Importing Modules
+import SmoothScroll from './modules/smooth-scroll.js';
+import AnimeWhenScroll from './modules/anime-when-scroll.js';
+import DropdownMenu from './modules/dropdown-menu.js';
+import MenuMobile from './modules/menu-mobile.js';
+
+// Smooth Scroll
+const smoothScroll = new SmoothScroll('[data-menu="smooth"] a[href^="#"]');
+smoothScroll.init();
+
+// Anime When Scroll
+const animeWhenScroll = new AnimeWhenScroll('[data-anime="scroll"]');
+animeWhenScroll.init();
+
+// Dropdown Menu
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
+
+// Menu Mobile
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
